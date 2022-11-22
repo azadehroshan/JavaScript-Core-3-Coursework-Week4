584a460324543f361fa568b15d22fde993b03a66
azadehroshan <105980239+azadehroshan@users.noreply.github.com>
2022-11-22 10:45:25 +0000

    get-average and car-sales-done

--- A-pass-tests/09-car-sales/car-sales.js
+++ A-pass-tests/09-car-sales/car-sales.js
@@ -1,3 +1,31 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+    let totals = {
+        Ford: 0,
+        Honda:0,
+        "Land Rover": 0,
+        Toyota: 0,
+    };
+    carsSold.forEach(car => {
+        if(car.make === "Honda"){
+            totals.Honda = totals.Honda + car.price ;
+        }
+        if(car.make ==="Ford"){
+            totals.Ford = totals.Ford + car.price;
+        } 
+        if(car.make ==="Land Rover"){
+            totals["Land Rover"]= totals["Land Rover"] + car.price;
+        }
+        if(car.make ==="Toyota"){
+            totals.Toyota = totals.Toyota + car.price;
+        } 
+    });
+    return totals;
+}
+
+
+
+
+
+
 
 module.exports = sales;
